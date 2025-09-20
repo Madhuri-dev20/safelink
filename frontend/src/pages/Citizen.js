f9c3a7c8008b680fa7cfe3930171582eac686394
Madhuri-dev20 <madagr54@in.ibm.com>
2025-09-20 16:18:40 -0500

    Update Citizen.js

--- frontend/src/pages/Citizen.js
+++ frontend/src/pages/Citizen.js
@@ -5,8 +5,11 @@ const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 export default function Citizen() {
   const [input, setInput] = useState("");
   const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const submitIncident = async () => {
+    setResponse("");
+    setLoading(true);
     const res = await fetch(`${API_URL}/incident`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -15,6 +18,7 @@ export default function Citizen() {
     const data = await res.json();
     setResponse(data);
     setInput("");
+    setLoading(false);
   };
 
   return (
@@ -29,9 +33,10 @@ export default function Citizen() {
       />
       <br />
       <button onClick={submitIncident}>Submit Incident</button>
-
-      {response && (
-        <div style={{ marginTop: 20 }}>
+<div style={{ marginTop: 20 }}>
+{loading?(
+  <p>Waiting for response...</p>):(response && (
+        
           <h3>Citizen Guidance</h3>
           <p>{response.citizen_guidance}</p>
 
@@ -39,6 +44,7 @@ export default function Citizen() {
           <p><b>Severity:</b> {response.severity}</p>
           <p>{response.responder_summary}</p>
         </div>
+ )
       )}
     </div>
   );
